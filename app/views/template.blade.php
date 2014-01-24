1dad89bfcf05eca563028fb92a22cf013061e868
Yeri Tiete <yeri@flatturtle.com>
2014-01-24 16:15:42 +0100

    shorter copyright

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -5,7 +5,7 @@
     <section id="social" class="block colorful">
         <div class="container">
             <div id="copyright">
-                Copyright &copy; {{ date('Y') }} <a href="http://rentalvalue.be">rentalVALUE.be</a> and <a href="http://flatturtle.com" target="_blank">FlatTurtle.com</a>. Some rights reserved.
+                Copyright &copy; {{ date('Y') }} <a href="http://rentalvalue.be">rentalVALUE</a> and <a href="http://flatturtle.com" target="_blank">FlatTurtle</a>. Some rights reserved.
             </div>
         </div>
     </section>
