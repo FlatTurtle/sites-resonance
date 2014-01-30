0af4d95560fe13d4e575989ab52ea86d6c7d683e
Yeri Tiete <yeri@flatturtle.com>
2014-01-30 11:31:41 +0100

    ft.fr

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -5,7 +5,7 @@
     <section id="social" class="block colorful">
         <div class="container">
             <div id="copyright">
-                Copyright &copy; {{ date('Y') }} <a href="http://rentalvalue.be">rentalVALUE</a> and <a href="http://flatturtle.com" target="_blank">FlatTurtle</a>. Some rights reserved. Images by ArtefactoryLab.
+                Copyright &copy; {{ date('Y') }} <a href="http://rentalvalue.be">rentalVALUE</a> and <a href="http://flatturtle.fr" target="_blank">FlatTurtle</a>. Some rights reserved. Images by ArtefactoryLab.
             </div>
         </div>
     </section>
