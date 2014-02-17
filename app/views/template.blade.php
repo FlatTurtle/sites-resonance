c534e68a7d5d2de9e5db4e4051d9180100fe07bf
Yeri Tiete <yeri@flatturtle.com>
2014-02-17 16:01:35 +0100

    copyright

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -5,7 +5,7 @@
     <section id="social" class="block colorful">
         <div class="container">
             <div id="copyright">
-                Copyright &copy; {{ date('Y') }} <a href="http://rentalvalue.be">rentalVALUE</a> and <a href="http://flatturtle.fr" target="_blank">FlatTurtle</a>. Some rights reserved. Images by ArtefactoryLab.
+                Copyright &copy; {{ date('Y') }} <a href="http://rentalvalue.be">rentalVALUE</a> and <a href="http://flatturtle.fr" target="_blank">FlatTurtle</a>. Some rights reserved. Images by <a href="http://www.artefactorylab.com/" target="_blank">ArtefactoryLab</a>. Document non contractuel.
             </div>
         </div>
     </section>
